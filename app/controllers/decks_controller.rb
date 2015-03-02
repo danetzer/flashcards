837110f9803069c1f997ce8df2386042d05b4d7d
dan <dan@capricalabs.com>
2015-03-02 12:47:41 -0800

    initialized repo

---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,45 @@
+class DecksController < ApplicationController
+  before_filter :authenticate 
+
+  def index
+    @decks = current_user.decks
+  end
+  
+  def show
+    @deck = find_deck
+    @cards = @deck.cards
+  end
+  
+  def new
+    @deck = current_user.decks.new
+  end
+  
+  def create
+    @deck = find_deck
+    @deck.save
+    
+    redirect_to decks_path
+  end
+  
+  def edit
+    @deck = find_deck
+  end
+  
+  def update
+    @deck = find_deck
+    @deck.update_attributes(params[:deck])
+    redirect_to deck_path(@deck.id)
+  end
+  
+  def destroy
+     @deck = find_deck
+     @deck.destroy
+     redirect_to decks_path
+  end
+  
+  private
+  
+  def find_deck
+    current_user.decks.find(params[:id])
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashcards::Application.config.secret_token = '4506ff55e665099103fa6f49cc115dc9449b1f0a43193d89ee1a8760141e7909634e23720caf16577df0b008fdb9f105bb6657c35b4df724367314afc808ee5c'
